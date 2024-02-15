543f9aad2bd74c9a301aac89490a0e3dadd9ff35
Shashank Daima <daimashashank10@gmail.com>
2024-02-16 05:03:54 +0530

    done basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
