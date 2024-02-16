1ebba04160f03468a3c3fb09f9ba818fc2c64a4f
Shashank Daima <daimashashank10@gmail.com>
2024-02-16 09:24:17 +0530

    minor middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { config } from './config'; // config.ts
 import { routes } from './routes/routes';
-// import { middleware } from './middleware'; // middleware.ts
-
+import notFoundMiddleware from './middlewares/notFound.middleware';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 const app = express();
 
 // app.use(middleware);
 app.use(express.json());
 app.use(routes);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 export { app };
